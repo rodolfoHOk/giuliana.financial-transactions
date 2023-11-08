07a69a1cb61909db42da8e5e769541506c740e49
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-07 22:44:13 -0300

    feat: change job to read the uploaded file

--- src/main/java/br/com/hiokdev/financialtransactions/job/BatchConfig.java
+++ src/main/java/br/com/hiokdev/financialtransactions/job/BatchConfig.java
@@ -6,6 +6,7 @@ import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -20,9 +21,10 @@ import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilde
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.Range;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -66,10 +68,13 @@ public class BatchConfig {
   }
 
   @Bean
-  public FlatFileItemReader<CNABTransaction> reader() {
+  @StepScope
+  public FlatFileItemReader<CNABTransaction> reader(
+    @Value("#{jobParameters['cnabFile']}") Resource resource
+  ) {
     return new FlatFileItemReaderBuilder<CNABTransaction>()
       .name("reader")
-      .resource(new FileSystemResource("files/CNAB.txt"))
+      .resource(resource)
       .fixedLength()
       .columns(
         new Range(1,1),
