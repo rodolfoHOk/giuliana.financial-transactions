32a9288e55973b89358984030fed7096b2c09eef
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-07 22:25:01 -0300

    feat: create job run exception

---
+++ src/main/java/br/com/hiokdev/financialtransactions/domain/exception/JobRunException.java
@@ -0,0 +1,9 @@
+package br.com.hiokdev.financialtransactions.domain.exception;
+
+public class JobRunException extends RuntimeException {
+
+  public JobRunException(String message) {
+    super(message);
+  }
+  
+}
