709dc6367a65cbbc84cafbd367216857ac689c47
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-07 22:24:42 -0300

    feat: create upload file exception

---
+++ src/main/java/br/com/hiokdev/financialtransactions/domain/exception/UploadFileException.java
@@ -0,0 +1,9 @@
+package br.com.hiokdev.financialtransactions.domain.exception;
+
+public class UploadFileException extends RuntimeException {
+
+  public UploadFileException(String message) {
+    super(message);
+  }
+  
+}
