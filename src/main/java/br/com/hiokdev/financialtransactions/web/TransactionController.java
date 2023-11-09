47bb84a93a7b8c3d94d7de2d64d6f09b39fdf7b1
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-09 20:26:42 -0300

    feat: create transaction controller

---
+++ src/main/java/br/com/hiokdev/financialtransactions/web/TransactionController.java
@@ -0,0 +1,27 @@
+package br.com.hiokdev.financialtransactions.web;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.hiokdev.financialtransactions.domain.entity.ReportTransaction;
+import br.com.hiokdev.financialtransactions.domain.entity.Transaction;
+import br.com.hiokdev.financialtransactions.domain.service.TransactionService;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionController {
+
+  private final TransactionService transactionService;
+  
+  public TransactionController(TransactionService transactionService) {
+    this.transactionService = transactionService;
+  }
+
+  @GetMapping
+  public Iterable<Transaction> listAll() {
+    return transactionService.listTotalsTransactionByStoreName();
+  }
+}
