d89d7d96b1070eb514803943a224844ae0f285a0
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-10 12:00:51 -0300

    feat: add order transaction method signature

--- src/main/java/br/com/hiokdev/financialtransactions/domain/repository/TransactionRepository.java
+++ src/main/java/br/com/hiokdev/financialtransactions/domain/repository/TransactionRepository.java
@@ -1,9 +1,11 @@
 package br.com.hiokdev.financialtransactions.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.hiokdev.financialtransactions.domain.entity.Transaction;
 
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-  
+  List<Transaction> findAllByOrderByStoreNameAscIdDesc();
 }
