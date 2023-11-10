a415eb2ed5aea6c63c5a234ecf75d3297a6a5f9c
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-10 12:05:01 -0300

    feat: implement grouping transactions for report in transaction service

--- src/main/java/br/com/hiokdev/financialtransactions/domain/service/TransactionService.java
+++ src/main/java/br/com/hiokdev/financialtransactions/domain/service/TransactionService.java
@@ -1,11 +1,16 @@
 package br.com.hiokdev.financialtransactions.domain.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import br.com.hiokdev.financialtransactions.domain.entity.ReportTransaction;
 import br.com.hiokdev.financialtransactions.domain.entity.Transaction;
+import br.com.hiokdev.financialtransactions.domain.entity.TransactionType;
 import br.com.hiokdev.financialtransactions.domain.repository.TransactionRepository;
 
 @Service
@@ -17,9 +22,29 @@ public class TransactionService {
     this.transactionRepository = transactionRepository;
   }
 
-  public Iterable<Transaction> listTotalsTransactionByStoreName() {
-    var transactions = transactionRepository.findAll();
-    return transactions;
+  public List<ReportTransaction> listTotalsTransactionByStoreName() {
+    var transactions = transactionRepository.findAllByOrderByStoreNameAscIdDesc();
+
+    Map<String, ReportTransaction> reportMap = new LinkedHashMap<String, ReportTransaction>();
+
+    transactions.forEach(transaction -> {
+      String storeName = transaction.storeName();
+      var transactionType = TransactionType.findByType(transaction.type());
+      BigDecimal amount = transaction.amount()
+        .multiply(BigDecimal.valueOf(transactionType.getSignal()));
+
+      reportMap.compute(storeName, (key, existingReport) -> {
+        var report = (existingReport != null) 
+          ? existingReport 
+          : new ReportTransaction(key, BigDecimal.ZERO, new ArrayList<Transaction>());
+
+          var transactionWithNewAmount = transaction.withAmount(amount);
+          return report.addTotal(amount).addTransaction(transactionWithNewAmount);
+      });
+
+    });
+
+    return new ArrayList<>(reportMap.values());
   }
 
 }
